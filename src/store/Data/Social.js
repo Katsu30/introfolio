fcd4709b19ac11bf7d26ec439bfc40e24e434a93
Katsuhiro <sc30.katsu@gmail.com>
2020-11-17 21:55:41 +0900

    .feat ファイル分割

---
+++ src/store/Data/Social.js
@@ -0,0 +1,12 @@
+export const socials = [
+  {
+    iconClasses: 'mdi-twitter',
+    url: 'https://twitter.com/katsu07_',
+    color: 'color: #1da1f2;'
+  },
+  {
+    iconClasses: 'mdi-github',
+    url: 'https://github.com/Katsu30',
+    color: 'color:#000000'
+  }
+]
